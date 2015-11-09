c9b1b8c665c558555b2e588d600947cae09aff2b
AInoob <ainoob@qq.com>
2015-11-09 14:48:48 -0600

    works now and adding commit

--- pyro.cpp
+++ pyro.cpp
@@ -1,29 +1,35 @@
+/*
+ * creating a list conatining every possible numbers, keep track of the number of possible permutations.
+ * Everytime get a new number, generate all possible permutations, and increment the number of possible permutations of those permutations in the list
+ * list[i] contains the number of possible permutions of i.
+ * everytime get all permutations x, increment list[x]
+ */
 #include <iostream>
 using namespace std;
 int main(){
-	int input[270000],map[270000],numOfInputs=0,n,a;
+	int input[250000],list[270000],numOfInputs=0,n,a,temp;
 	for(int i=0;i<270000;i++){
-		map[i]=0;
-	}
-	for(int i=0;i<18;i++){
-		map[1<<i]=1;
-		for(int j=i+1;j<18;j++){
-			map[(1<<i)+(1<<j)]=1;
-		}
+		list[i]=0;
 	}
 	cin>>n;
 	while(n!=-1){
 		input[numOfInputs++]=n;
+		for(int j=0;j<18;j++){
+			temp=n^(1<<j);
+			if(temp<n){
+				list[temp]++;
+			}
+			for(int k=j+1;k<18;k++){
+				temp=n^(1<<j)^(1<<k);
+				if(temp<n){
+					list[temp]++;
+				}
+			}
+		}
 		cin>>n;
 	}
 	for(int i=0;i<numOfInputs;i++){
-		a=0;
-		for(int j=i+1;j<numOfInputs;j++){
-			if(map[input[i] xor input[j]]){
-				a++;
-			}
-		}
-		cout<<input[i]<<":"<<a<<endl;
+		cout<<input[i]<<":"<<list[input[i]]<<endl;
 	}
 	return 0;
 }
